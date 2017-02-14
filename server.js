a6c3e4f9b988d9656013ae614f6d5fc42f43b18b
zhibin.zhang@outlook.com <zhibin.zhang@outlook.com>
2017-02-14 00:44:21 -0800

    a toy program

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Created by zhibin_zhang_MacPro15 on 2/12/17.
+ */
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.set('view engine', 'ejs');
+
+app.locals.resoucePath = "/";
+
+/*
+app.get('/', function(req, res){
+  res.sendfile('views/index.html');
+});
+*/
+
+app.set("port", process.env.PORT || 3000);
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
+app.listen(app.get("port"), function() {
+  console.log("App started on port " + app.get("port"));
+});
